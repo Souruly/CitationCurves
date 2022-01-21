35ba1d5784f9a506022b902e244f3ad2505c39bd
Sourav Kulkarni <souruly@gmail.com>
2022-01-21 14:30:12 +0100

    Clustering somehow works...

--- cluster.py
+++ cluster.py
@@ -1,3 +1,5 @@
+# Code reference : https://tslearn.readthedocs.io/en/stable/auto_examples/clustering/plot_kmeans.html
+
 import numpy
 from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
@@ -8,13 +10,9 @@ from tslearn.clustering import TimeSeriesKMeans
 from tslearn.utils import to_time_series_dataset
 from tslearn.preprocessing import TimeSeriesScalerMeanVariance, TimeSeriesResampler
 
-
-# Check validity of clusters, cluster score
-# Find optimal number of clusters
-
 papers = []
 timeSeriesDataset = []
-maxSelect = 1000
+maxSelect = 100
 
 with open('dataset.csv', mode='r') as dataFile:
     index = 0
@@ -25,7 +23,7 @@ with open('dataset.csv', mode='r') as dataFile:
         # papers.append(md)
 
         thisPaper = [float(freq) for freq in line[2:]].copy()
-        timeSeriesDataset.append(thisPaper)
+        timeSeriesDataset.append(thisPaper.copy())
 
         thisPaper.insert(0, line[1])
         thisPaper.insert(0, line[0])
@@ -35,22 +33,75 @@ with open('dataset.csv', mode='r') as dataFile:
         if count>maxSelect:
             break
 
+
+seed = 0
+numpy.random.seed(seed)
 X_train = to_time_series_dataset(timeSeriesDataset)
-# # X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
-# X_train = TimeSeriesResampler(sz=10).fit_transform(X_train)
-# sz = X_train.shape[1]
 
-print("Data In")
-print("Processing...")
-km = TimeSeriesKMeans(n_clusters=7, metric="dtw")
-labels = km.fit_predict(X_train)
-print("Processed")
+# Resample Series
+X_train = TimeSeriesResampler(sz=40).fit_transform(X_train)
+sz = X_train.shape[1]
 
-for i in range(0, len(papers)):
-    papers[i].insert(0, labels[i])
+# Euclidean k-means
+print("Euclidean k-means")
+km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=seed)
+y_pred = km.fit_predict(X_train)
 
-with open('output.csv', 'w', newline='') as f: 
-        write = csv.writer(f) 
-        write.writerows(papers) 
+plt.figure()
+for yi in range(3):
+    plt.subplot(3, 3, yi + 1)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("Euclidean $k$-means")
 
+# DBA-k-means
+print("DBA k-means")
+dba_km = TimeSeriesKMeans(n_clusters=3,
+                          n_init=2,
+                          metric="dtw",
+                          verbose=True,
+                          max_iter_barycenter=10,
+                          random_state=seed)
+y_pred = dba_km.fit_predict(X_train)
 
+for yi in range(3):
+    plt.subplot(3, 3, 4 + yi)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(dba_km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("DBA $k$-means")
+
+# Soft-DTW-k-means
+print("Soft-DTW k-means")
+sdtw_km = TimeSeriesKMeans(n_clusters=3,
+                           metric="softdtw",
+                           metric_params={"gamma": .01},
+                           verbose=True,
+                           random_state=seed)
+y_pred = sdtw_km.fit_predict(X_train)
+
+for yi in range(3):
+    plt.subplot(3, 3, 7 + yi)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(sdtw_km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("Soft-DTW $k$-means")
+
+plt.tight_layout()
+plt.show()
